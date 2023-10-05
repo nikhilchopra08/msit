28f13938c4a2a2bf955631d8d75a73b6aadbb02f
nikhilchopra08 <129540378+nikhilchopra08@users.noreply.github.com>
2023-10-05 21:52:23 +0530

    javascript

--- script1.js
+++ script1.js
@@ -1,76 +1,76 @@
-// // Sample data for tasks (you can replace this with actual data)
-// const initialTasks = [
-//     { id: 1, name: "Task 1", completed: false },
-//     { id: 2, name: "Task 2", completed: false },
-// ];
+// Sample data for tasks (you can replace this with actual data)
+const initialTasks = [
+    { id: 1, name: "Task 1", completed: false },
+    { id: 2, name: "Task 2", completed: false },
+];
 
-// // Function to render tasks on the page
-// function renderTasks(tasks) {
-//     const taskList = document.getElementById("taskList");
-//     taskList.innerHTML = "";
+// Function to render tasks on the page
+function renderTasks(tasks) {
+    const taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
 
-//     tasks.forEach(task => {
-//         const listItem = document.createElement("li");
-//         listItem.innerHTML = `
-//             <input type="checkbox" id="task${task.id}" ${task.completed ? 'checked' : ''}>
-//             <label for="task${task.id}">${task.name}</label>
-//             <button class="edit">Edit</button>
-//             <button class="delete">Delete</button>
-//         `;
+    tasks.forEach(task => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `
+            <input type="checkbox" id="task${task.id}" ${task.completed ? 'checked' : ''}>
+            <label for="task${task.id}">${task.name}</label>
+            <button class="edit">Edit</button>
+            <button class="delete">Delete</button>
+        `;
 
-//         // Add event listeners for editing and deleting tasks
-//         const editButton = listItem.querySelector(".edit");
-//         const deleteButton = listItem.querySelector(".delete");
+        // Add event listeners for editing and deleting tasks
+        const editButton = listItem.querySelector(".edit");
+        const deleteButton = listItem.querySelector(".delete");
 
-//         editButton.addEventListener("click", () => editTask(task.id));
-//         deleteButton.addEventListener("click", () => deleteTask(task.id));
+        editButton.addEventListener("click", () => editTask(task.id));
+        deleteButton.addEventListener("click", () => deleteTask(task.id));
 
-//         taskList.appendChild(listItem);
-//     });
-// }
+        taskList.appendChild(listItem);
+    });
+}
 
-// // Function to add a new task
-// function addTask(taskName) {
-//     const newTask = {
-//         id: Date.now(),
-//         name: taskName,
-//         completed: false,
-//     };
-//     tasks.push(newTask);
-//     renderTasks(tasks);
-// }
+// Function to add a new task
+function addTask(taskName) {
+    const newTask = {
+        id: Date.now(),
+        name: taskName,
+        completed: false,
+    };
+    tasks.push(newTask);
+    renderTasks(tasks);
+}
 
-// // Function to edit a task
-// function editTask(taskId) {
-//     const taskToEdit = tasks.find(task => task.id === taskId);
-//     if (!taskToEdit) return;
+// Function to edit a task
+function editTask(taskId) {
+    const taskToEdit = tasks.find(task => task.id === taskId);
+    if (!taskToEdit) return;
 
-//     const newName = prompt("Edit task name:", taskToEdit.name);
-//     if (newName !== null) {
-//         taskToEdit.name = newName;
-//         renderTasks(tasks);
-//     }
-// }
+    const newName = prompt("Edit task name:", taskToEdit.name);
+    if (newName !== null) {
+        taskToEdit.name = newName;
+        renderTasks(tasks);
+    }
+}
 
-// // Function to delete a task
-// function deleteTask(taskId) {
-//     const taskIndex = tasks.findIndex(task => task.id === taskId);
-//     if (taskIndex !== -1) {
-//         tasks.splice(taskIndex, 1);
-//         renderTasks(tasks);
-//     }
-// }
+// Function to delete a task
+function deleteTask(taskId) {
+    const taskIndex = tasks.findIndex(task => task.id === taskId);
+    if (taskIndex !== -1) {
+        tasks.splice(taskIndex, 1);
+        renderTasks(tasks);
+    }
+}
 
-// // Function to handle form submission
-// document.getElementById("addTaskForm").addEventListener("submit", function (e) {
-//     e.preventDefault();
-//     const taskName = document.getElementById("taskName").value.trim();
-//     if (taskName !== "") {
-//         addTask(taskName);
-//         document.getElementById("taskName").value = ""; // Clear the input field
-//     }
-// });
+// Function to handle form submission
+document.getElementById("addTaskForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const taskName = document.getElementById("taskName").value.trim();
+    if (taskName !== "") {
+        addTask(taskName);
+        document.getElementById("taskName").value = ""; // Clear the input field
+    }
+});
 
-// // Initial rendering of tasks
-// const tasks = [...initialTasks];
-// renderTasks(tasks);
+// Initial rendering of tasks
+const tasks = [...initialTasks];
+renderTasks(tasks);
--- scriptD.js
+++ scriptD.js
@@ -1,41 +1,41 @@
-// // Sample data for the selected task (replace this with actual data)
-// const selectedTask = {
-//     name: "Sample Task",
-//     description: "This is a sample task description.",
-//     dueDate: "January 1, 2024",
-//     status: "Incomplete",
-// };
+// Sample data for the selected task (replace this with actual data)
+const selectedTask = {
+    name: "Sample Task",
+    description: "This is a sample task description.",
+    dueDate: "January 1, 2024",
+    status: "Incomplete",
+};
 
-// // Function to populate task details
-// function populateTaskDetails() {
-//     document.querySelector('.task-details h2').textContent = selectedTask.name;
-//     document.querySelector('.task-details p').textContent = selectedTask.description;
-//     document.getElementById('dueDate').textContent = selectedTask.dueDate;
-//     document.getElementById('taskStatus').textContent = selectedTask.status;
-// }
+// Function to populate task details
+function populateTaskDetails() {
+    document.querySelector('.task-details h2').textContent = selectedTask.name;
+    document.querySelector('.task-details p').textContent = selectedTask.description;
+    document.getElementById('dueDate').textContent = selectedTask.dueDate;
+    document.getElementById('taskStatus').textContent = selectedTask.status;
+}
 
-// // Function to mark the task as complete
-// document.getElementById('markAsComplete').addEventListener('click', () => {
-//     selectedTask.status = 'Complete';
-//     document.getElementById('taskStatus').textContent = selectedTask.status;
-// });
+// Function to mark the task as complete
+document.getElementById('markAsComplete').addEventListener('click', () => {
+    selectedTask.status = 'Complete';
+    document.getElementById('taskStatus').textContent = selectedTask.status;
+});
 
-// // Function to handle editing the task (redirect to edit page or show an edit form)
-// document.getElementById('editTask').addEventListener('click', () => {
-//     // Implement the edit functionality here, such as redirecting to an edit page or displaying an edit form.
-//     // You can use window.location.href to redirect or toggle the visibility of an edit form.
-//     // Example: window.location.href = 'edit-task.html?id=' + selectedTask.id;
-// });
+// Function to handle editing the task (redirect to edit page or show an edit form)
+document.getElementById('editTask').addEventListener('click', () => {
+    // Implement the edit functionality here, such as redirecting to an edit page or displaying an edit form.
+    // You can use window.location.href to redirect or toggle the visibility of an edit form.
+    // Example: window.location.href = 'edit-task.html?id=' + selectedTask.id;
+});
 
-// // Function to handle deleting the task (redirect to task list or show a confirmation dialog)
-// document.getElementById('deleteTask').addEventListener('click', () => {
-//     // Implement the delete functionality here, such as redirecting to the task list or displaying a confirmation dialog.
-//     // You can use window.location.href to redirect or show a confirmation dialog using JavaScript.
-//     // Example: const confirmDelete = confirm('Are you sure you want to delete this task?');
-//     // if (confirmDelete) {
-//     //     window.location.href = 'task-list.html';
-//     // }
-// });
+// Function to handle deleting the task (redirect to task list or show a confirmation dialog)
+document.getElementById('deleteTask').addEventListener('click', () => {
+    // Implement the delete functionality here, such as redirecting to the task list or displaying a confirmation dialog.
+    // You can use window.location.href to redirect or show a confirmation dialog using JavaScript.
+    // Example: const confirmDelete = confirm('Are you sure you want to delete this task?');
+    // if (confirmDelete) {
+    //     window.location.href = 'task-list.html';
+    // }
+});
 
-// // Populate task details on page load
-// populateTaskDetails();
+// Populate task details on page load
+populateTaskDetails();
